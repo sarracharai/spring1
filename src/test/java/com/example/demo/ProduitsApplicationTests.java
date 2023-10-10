d7a3e572e8332352a8f3ebbebb02411054c5dc77
sarra <sarracharai123@gmail.com>
2023-10-10 13:40:14 +0200

    first commit

---
+++ src/test/java/com/example/demo/ProduitsApplicationTests.java
@@ -0,0 +1,93 @@
+package com.example.demo;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.example.demo.entities.Produit;
+import com.example.demo.repos.ProduitRepository;
+
+@SpringBootTest
+class ProduitsApplicationTests {
+	
+	@Autowired 
+	private ProduitRepository produitRepository; //injection de dependences
+	
+	@Test
+	public void testCreateProduit() {
+	Produit prod = new Produit("PC Lenovo",2200.500,new Date());
+	produitRepository.save(prod);
+	}
+	
+	@Test
+	public void testFindProduit()
+	{
+	Produit p = produitRepository.findById(1L).get();
+	System.out.println(p);
+	}
+	
+	@Test
+	public void testUpdateProduit()
+	{
+	Produit p = produitRepository.findById(1L).get();
+	p.setPrixProduit(2000.0);
+	produitRepository.save(p);
+	System.out.println(p);
+	}
+	
+	
+	@Test
+	public void testDeleteProduit()
+	{
+	produitRepository.deleteById(1L);;
+	}
+	
+	@Test
+	public void testFindAllProduits()
+	{
+	List<Produit> prods = produitRepository.findAll();
+	for (Produit p : prods)
+	{
+	System.out.println(p);
+	}
+
+	}
+	
+	
+	@Test
+	public void testFindProduitByNom()
+	{
+	List<Produit> prods = produitRepository.findByNomProduit("PC Lenovo");
+	for (Produit p : prods)
+	{
+	System.out.println(p);
+	}
+	}
+	
+	
+	@Test
+	public void testFindByNomProduitContains ()
+	{
+	List<Produit> prods=produitRepository.findByNomProduitContains("P");
+	for (Produit p : prods)
+	{
+	System.out.println(p);
+	} 
+	}
+	
+	
+	@Test
+	public void testfindByNomPrix()
+	{
+	List<Produit> prods = produitRepository.findByNomPrix("PC Lenovo", 1000.0);
+	for (Produit p : prods)
+	{
+	System.out.println(p);
+	}
+	}
+
+
+}
+
